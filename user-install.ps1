63019fc800698e09d277ee351f18fd1448a2997a
Jan "JoubaMety" Rašnovský <56211361+JoubaMety@users.noreply.github.com>
2025-09-03 14:09:48 +0200

    powershell script for simple install
    fixed

--- user-install.ps1
+++ user-install.ps1
@@ -1,10 +1,18 @@
 $_originalContext = Get-Location
 function Write-Message {
     param (
-        [string]$Message
+        [string]$Message,
+        [string]$Level
     )
-    Write-Host "[momoiPlus] " -NoNewline -ForegroundColor Yellow
-    Write-Host $Message
+    $color = switch ($Level) {
+        default { "White" }
+        "Success" { "Green" }
+        "Info" { "Blue" }
+        "Warning" { "Yellow" }
+        "Error" { "Red" }
+    }
+    Write-Host "[MomoiPlus] " -NoNewline -ForegroundColor Yellow
+    Write-Host $Message -ForegroundColor $color
 }
 # check if OS is Windows
 if ($env:OS -ne "Windows_NT") {
@@ -12,115 +20,175 @@ if ($env:OS -ne "Windows_NT") {
     exit
 }
 
+$defaultRootPath = $env:TMP
+# check if enough space on the disk, get letter from $defaultRootPath
+$driveLetter = $defaultRootPath.Substring(0, 1)
+$freeSpace = (Get-PSDrive -Name $driveLetter).Free
+$requiredSpace = 900MB
+if ($freeSpace -lt $requiredSpace) {
+    Write-Message "Not enough disk space available. At least 900MB is required." -Level "Error"
+    exit
+}
+
 $links = @{
     "vencordGit"        = "https://github.com/Vendicated/Vencord.git"
-    "vencordRelease"    = "https://github.com/Vendicated/Vencord/archive/refs/heads/main.zip"
     "momoiPlusGit"      = "https://github.com/ddddjBlue/vencord-momoi-plus.git"
-    "momoiPlusRelease"  = "https://github.com/ddddjBlue/vencord-momoi-plus/archive/refs/heads/main.zip"
-    "pnpmInstallScript" = "https://get.pnpm.io/install.ps1"
+    "minGit"            = "https://github.com/git-for-windows/git/releases/download/v2.51.0.windows.1/MinGit-2.51.0-64-bit.zip"
+    "node"              = "https://nodejs.org/dist/v22.19.0/node-v22.19.0-win-x64.zip"
+    "pnpm"              = "https://github.com/pnpm/pnpm/releases/download/v10.15.1/pnpm-win-x64.exe"
 }
-$defaultInstallPath = Join-Path $env:TMP "vencord-momoi-plus"
+$defaultInstallPath = Join-Path $defaultRootPath "vencord-momoi-plus"
 $momoiPlusPath = Join-Path -Path $defaultInstallPath "\src\userplugins\vencord-momoi-plus"
 
 $gitInstalled = Get-Command git -ErrorAction SilentlyContinue
 $pnpmInstalled = Get-Command pnpm -ErrorAction SilentlyContinue
+$nodeInstalled = Get-Command node -ErrorAction SilentlyContinue
 
-if (-not ($pnpmInstalled)) {
+# Download pnpm if not installed
+if (-not ($pnpmInstalled) -and -not (Test-Path -Path "$defaultRootPath\pnpm")) {
     Write-Message "pnpm is not installed. Installing pnpm..."
-    Invoke-WebRequest -Uri $links.pnpmInstallScript -UseBasicParsing | Invoke-Expression
+    $null = New-Item -ItemType Directory -Path "$defaultRootPath\pnpm"
+    Invoke-WebRequest -Uri $links.pnpm -OutFile "$defaultRootPath\pnpm\pnpm.exe"
+    $env:Path += ";$defaultRootPath\pnpm"
+    # Test
     $pnpmInstalled = Get-Command pnpm -ErrorAction SilentlyContinue
     if ($pnpmInstalled) {
-        Write-Message "pnpm installed successfully."
-    }
-    else {
-        Write-Message "Failed to install pnpm. Please install it manually and re-run the script. (https://pnpm.io/installation#)"
+        Write-Message "pnpm installation successful." -Level "Success"
+    } else {
+        Write-Message "pnpm installation failed." -Level "Error"
         exit
     }
 }
 else {
-    Write-Message "pnpm is already installed."
+    if ($env:Path -notlike "*$defaultRootPath\pnpm*") {
+        $env:Path += ";$defaultRootPath\pnpm"
+    }
+    Write-Message "pnpm is already installed. Skipping."
 }
+
+# Download MinGit if no git is installed
+if (-not ($gitInstalled) -and -not (Test-Path -Path "$defaultRootPath\MinGit")) {
+    Write-Message "Git is not installed. Installing MinGit..."
+    Invoke-WebRequest -Uri $links.minGit -OutFile "$defaultRootPath\MinGit.zip"
+    $null = New-Item -ItemType Directory -Path "$defaultRootPath\MinGit"
+    Expand-Archive -Path "$defaultRootPath\MinGit.zip" -DestinationPath "$defaultRootPath\MinGit\" -Force
+    Remove-Item -Path "$defaultRootPath\MinGit.zip" -Force
+    $env:Path += ";$defaultRootPath\MinGit\mingw64\bin"
+    $gitInstalled = Get-Command git -ErrorAction SilentlyContinue
+    if ($gitInstalled) {
+        Write-Message "MinGit installation successful." -Level "Success"
+    } else {
+        Write-Message "MinGit installation failed." -Level "Error"
+        exit
+    }
+} else {
+    if ($env:Path -notlike "*$defaultRootPath\MinGit\mingw64\bin*") {
+        $env:Path += ";$defaultRootPath\MinGit\mingw64\bin"
+    }
+    Write-Message "Git is already installed."
+}
+
+# Download node if no node is installed
+if (-not ($nodeInstalled) -and -not (Test-Path -Path "$defaultRootPath\node")) {
+    Write-Message "Node.js is not installed. Installing Node.js..."
+    Invoke-WebRequest -Uri $links.node -OutFile "$defaultRootPath\node.zip"
+    # create node folder
+    $null = New-Item -ItemType Directory -Path "$defaultRootPath\node"
+    Expand-Archive -Path "$defaultRootPath\node.zip" -DestinationPath "$defaultRootPath\node\" -Force
+    Remove-Item -Path "$defaultRootPath\node.zip" -Force
+    $folderName = $links.node.Split("/")[-1].Replace(".zip", "")
+    $null = Move-Item -Path "$defaultRootPath\node\$folderName\*" -Destination "$defaultRootPath\node"
+    $env:Path += ";$defaultRootPath\node"
+    # Test
+    $nodeInstalled = Get-Command node -ErrorAction SilentlyContinue
+    if ($nodeInstalled) {
+        Write-Message "Node.js installation successful." -Level "Success"
+    } else {
+        Write-Message "Node.js installation failed." -Level "Error"
+        exit
+    }
+} else {
+    if ($env:Path -notlike "*$defaultRootPath\node*") {
+        $env:Path += ";$defaultRootPath\node"
+    }
+    Write-Message "Node.js is already installed. Skipping."
+}
+
 # if defaultInstallPath folder doesn't exist, create one
 if (-not (Test-Path -Path $defaultInstallPath)) {
-    if ($gitInstalled) {
-        git clone $links.vencordGit $defaultInstallPath
-        Write-Message "Cloned Vencord repository to $defaultInstallPath"
-    }
-    else {
-        New-Item -ItemType Directory -Path $defaultInstallPath
-        Write-Message "Created directory $defaultInstallPath"
-    }
-}
-# If git is not installed, download the release zip and extract it to /dist
-if (-not ($gitInstalled)) {
-    # Download VencordRelease
-    Invoke-WebRequest -Uri $links.vencordRelease -OutFile "$defaultInstallPath\VencordRelease.zip"
-    # Extract VencordRelease
-    Expand-Archive -Path "$defaultInstallPath\VencordRelease.zip" -DestinationPath "$defaultInstallPath\dist" -Force
-    Remove-Item -Path "$defaultInstallPath\VencordRelease.zip" -Force
-    # Test if extraction was successful
-    if (Test-Path -Path "$defaultInstallPath\dist\src") {
-        Write-Message "Vencord source files were successfully extracted."
-    }
-    else {
-        Write-Message "Failed to extract Vencord source files."
+    Write-Message "Cloning Vencord repository to $defaultInstallPath..."
+    git clone $links.vencordGit $defaultInstallPath
+    # Test
+    if (-not (Test-Path -Path $defaultInstallPath)) {
+        Write-Message "Failed to clone Vencord repository." -Level "Error"
         exit
     }
-}
-else {
+    Write-Message "Cloned Vencord repository to $defaultInstallPath" -Level "Success"
+} else {
     # If git is installed, pull the latest changes
+    Write-Message "Vencord repository already exists. Pulling latest changes..."
     Set-Location -Path $defaultInstallPath
     git pull origin main
+    # Test via checking if previous command was successful
+    if ($?) {
+        Write-Message "Vencord repository updated successfully." -Level "Success"
+    } else {
+        Write-Message "Failed to update Vencord repository." -Level "Error"
+    }
     Set-Location -Path $_originalContext
 }
 
-
 # Check if folder userplugins exists
 if (-not (Test-Path -Path "$defaultInstallPath\src\userplugins")) {
-    New-Item -ItemType Directory -Path "$defaultInstallPath\src\userplugins"
+    $null = New-Item -ItemType Directory -Path "$defaultInstallPath\src\userplugins"
     Write-Message "Created directory $defaultInstallPath\src\userplugins"
 }
 # Check if momoiPlus is already installed
 if (-not (Test-Path -Path $momoiPlusPath)) {
-    if ($gitInstalled) {
-        git clone $links.momoiPlusGit $momoiPlusPath
-        Write-Message "Cloned MomoiPlus repository to $momoiPlusPath"
-    }
-    else {
-        New-Item -ItemType Directory -Path $momoiPlusPath
-        Write-Message "Created directory $momoiPlusPath"
-        Invoke-WebRequest -Uri $links.momoiPlusRelease -OutFile "$momoiPlusPath\MomoiPlusRelease.zip"
-        Expand-Archive -Path "$momoiPlusPath\MomoiPlusRelease.zip" -DestinationPath "$momoiPlusPath" -Force
-        Remove-Item -Path "$momoiPlusPath\MomoiPlusRelease.zip" -Force
-    }
+    Write-Message "Cloning MomoiPlus repository to $momoiPlusPath..."
+    git clone $links.momoiPlusGit $momoiPlusPath
+    Write-Message "Cloned MomoiPlus repository to $momoiPlusPath" -Level "Success"
 }
 else {
-    # If momoiPlus is already installed, pull the latest changes
-    if ($gitInstalled) {
-        Set-Location -Path $momoiPlusPath
-        git pull origin main
-        Set-Location -Path $_originalContext
+    Write-Message "MomoiPlus repository already exists. Pulling latest changes..."
+    Set-Location -Path $momoiPlusPath
+    git pull origin main
+    if ($?) {
+        Write-Message "MomoiPlus repository updated successfully." -Level "Success"
+    } else {
+        Write-Message "Failed to update MomoiPlus repository." -Level "Warning"
     }
-    else {
-        Invoke-WebRequest -Uri $links.momoiPlusRelease -OutFile "$momoiPlusPath\MomoiPlusRelease.zip"
-        Expand-Archive -Path "$momoiPlusPath\MomoiPlusRelease.zip" -DestinationPath "$momoiPlusPath" -Force
-        Remove-Item -Path "$momoiPlusPath\MomoiPlusRelease.zip" -Force
-    }
-    Write-Message "MomoiPlus updated successfully."
+    Set-Location -Path $_originalContext
 }
 # Install dependencies using pnpm
 Set-Location -Path "$defaultInstallPath"
 Write-Message "Installing dependencies using pnpm..."
 pnpm install
-Write-Message "Dependencies installed successfully."
+if ($?) {
+    Write-Message "Dependencies installed successfully." -Level "Success"
+} else {
+    Write-Message "Failed to install dependencies." -Level "Error"
+    exit
+}
 # Build Vencord
 Write-Message "Building Vencord..."
 pnpm build
-Write-Message "Vencord built successfully."
+if ($?) {
+    Write-Message "Vencord built successfully." -Level "Success"
+} else {
+    Write-Message "Failed to build Vencord." -Level "Error"
+    exit
+}
 # Inject Vencord
 Write-Message "Injecting Vencord..."
 pnpm inject
-Write-Message "Vencord injected successfully."
-Write-Host "Life... is Kuyashi."
+if ($?) {
+    Write-Message "Vencord injected successfully." -Level "Success"
+} else {
+    Write-Message "Failed to inject Vencord." -Level "Error"
+}
+Write-Message "Discord was shut off during injection, you will have to start it up manually." -Level "Warning"
+Write-Message "To enable MomoiPlus plugin, go to Settings > Plugins > MomoiPlus, restart Discord and enjoy." -Level "Info"
+Write-Host "Life... is Kuyashi." -ForegroundColor Magenta
 # Return to original location
 Set-Location -Path $_originalContext
